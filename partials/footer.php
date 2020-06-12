bbffc99817ba0524d356faa8c25140750a0f13c8
kadev0502 <kadev0502@gmail.com>
2020-06-12 16:43:13 +0200

    ajout de jquery et mise en place des fichiers js

--- partials/footer.php
+++ partials/footer.php
@@ -4,11 +4,11 @@
    <!-- SCRIPT CONTENT -->
    <!-- ====================== -->
    <!-- jQuery LINK -->
-          <script src = "../js/jquery-3.4.1.min.js"></script>
+          <script src = "js/jquery-3.4.1.min.js"></script>
 
    <!-- JS BOOSTRAP LINK  -->
-          <script src = "../js/bootstrap.min.js"></script>
+          <script src = "js/bootstrap.min.js"></script>
 
    <!-- PERSONAL JS LINK  -->
-          <script src = "../js/script.js"></script>
+          <script src = "js/script.js"></script>
       </footer>
